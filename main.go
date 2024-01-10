7474a9b7d91b98b5d07c065145e81713c6fc75ef
Jay-02 <jawadalamoush02@gmail.com>
2024-01-10 20:20:24 +0300

    Added panels for all types of tasks

--- main.go
+++ main.go
@@ -1,67 +1,99 @@
 package main
 import(
-"fmt"
-"os"
-tea "github.com/charmbracelet/bubbletea"
+	"fmt"
+	"os"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/lipgloss"
 )
 type status int
+const divisor = 4
 const (
-todo status = iota
-inProgress
-done
+	todo status = iota
+	inProgress
+	done
+
 )
 type Task struct{
-status status
-title string
-description string
+	status status
+	title string
+	description string
+
 }
 func (t Task) FilterValue() string{
-    return t.title
-    }
+	return t.title
+}
 func (t Task) Title() string {
-    return t.title
-    }
+	return t.title
+}
 
 func (t Task) Description() string {
-return t.description
+	return t.description
 }
 type Model struct{
-list list.Model
-err error
+	focused status
+	lists []list.Model
+	err error
+	loaded bool
 }
 func New() *Model{
-    return &Model{}
+	return &Model{}
 }
-func (m *Model) initList(width, height int){
-    m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
-    m.list.Title = "To Do"
-    m.list.SetItems([]list.Item{
-    Task{ status: todo, title: "meow", description: "cat"},
-    Task{ status: inProgress, title: "woof", description: "dog"},
+func (m *Model) initLists(width, height int){
+	defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width/divisor, height)
+	defaultList.SetShowHelp(false)
+	m.lists = []list.Model{defaultList, defaultList, defaultList}
+	//Init To Do
+	m.lists[todo].Title = "To Do"
+	m.lists[todo].SetItems([]list.Item{
+		Task{ status: todo, title: "meow", description: "cat"},
+		Task{ status:todo, title: "woof", description: "dog"},
 
-        })
+	})
+	//Init In progress
+	m.lists[inProgress].Title = "In Progress"
+	m.lists[inProgress].SetItems([]list.Item{
+		Task{ status: inProgress, title: "meow", description: "cat"},
+		Task{ status: inProgress, title: "woof", description: "dog"},
+
+	})
+	//Init done
+	m.lists[todo].Title = "In Progress"
+	m.lists[done].SetItems([]list.Item{
+		Task{ status: done, title: "meow", description: "cat"},
+		Task{ status: done, title: "woof", description: "dog"},
+
+	})
 }
 func (m Model) Init() tea.Cmd{
-    return nil
+	return nil
 }
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd){
-    switch msg:=msg.(type){
-    case tea.WindowSizeMsg:
-        m.initList(msg.Width, msg.Height)
-    }
-    var cmd tea.Cmd
-    m.list, cmd = m.list.Update(msg)
-     return m, cmd
+	switch msg:=msg.(type){
+	case tea.WindowSizeMsg:
+		if !m.loaded{
+			m.initLists(msg.Width, msg.Height)
+			m.loaded = true
+
+		}
+
+	}
+	var cmd tea.Cmd
+	m.lists[m.focused], cmd = m.lists[m.focused].Update(msg)
+	return m, cmd
 }
 func (m Model) View() string{
-    return m.list.View()
+	if m.loaded{
+		return lipgloss.JoinHorizontal(lipgloss.Left, m.lists[todo].View(), m.lists[inProgress].View(), m.lists[done].View())
+	} else{
+		return "loading"
+	}
 }
 func main(){
-    m:=New()
-    p:=tea.NewProgram(m)
-    if err:=p.Start();err !=nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	m:=New()
+	p:=tea.NewProgram(m)
+	if err:=p.Start();err !=nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
